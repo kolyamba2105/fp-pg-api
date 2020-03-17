18dc205fc7242be6f182d1097aa6375f8eda887b
mharavetskyy <mharavetskyy@evojam.com>
2020-03-17 11:37:25 +0100

    Dumb change

--- src/app.ts
+++ src/app.ts
@@ -4,11 +4,12 @@ import shoppingLists from 'api/shopping-lists/shopping-list.router'
 import users from 'api/users/user.router'
 import { sequelize, sync } from 'db'
 import express from 'express'
+import { console } from 'fp-ts'
 import morgan from 'morgan'
 
 const app = express()
 
-sync(sequelize)().then(console.log)
+sync(sequelize)().then(_ => console.log(_)())
 
 app.use(express.json())
 app.use(morgan('dev'))
@@ -20,4 +21,4 @@ app.use('/users', users)
 
 const port = process.env.PORT || 3000
 
-app.listen(port, () => console.log(`Listening on port ${port}...`))
+app.listen(port, console.log(`Listening on port ${port}...`))
